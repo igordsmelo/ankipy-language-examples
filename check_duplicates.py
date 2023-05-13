36849c8b30d14fd8436935f9dde8a6b797cc4bb0
igordsmelo <102251050+igordsmelo@users.noreply.github.com>
2023-05-13 12:25:00 -0300

    updates
    Signed-off-by: igordsmelo <102251050+igordsmelo@users.noreply.github.com>

--- check_duplicates.py
+++ check_duplicates.py
@@ -1,1 +1,4 @@
 # for notes/cards with the tag "Languages::Japanese::Core6K"
+# 6K = [notes for notes in tag "Languages::Japanese::Core6K"]
+# Main = [notes for notes in "Languages::Japanese::Main"]
+# duplicates = [notes['Word'] for notes in 6K if notes['Word'] in Main]
