e46d6939aba4c19862a2a804a7b5cd4d3fad4639
igordsmelo <102251050+igordsmelo@users.noreply.github.com>
2023-05-13 12:50:40 -0300

    updates
    Signed-off-by: igordsmelo <102251050+igordsmelo@users.noreply.github.com>

--- anki_language_samples/anki_sentences.py
+++ anki_language_samples/anki_sentences.py
@@ -65,16 +65,17 @@ def filter_note_by_tag(notes, include=None, exclude=None):
     return filtered_notes
 
 
-queries = ["deck:Default::Downloaded::JLPT::N5",
-           "deck:Default::Downloaded::JLPT::N4",
-           "deck:Default::Downloaded::JLPT::N3",
-           "deck:Default::Downloaded::JLPT::N2",
-           "deck:Default::Downloaded::JLPT::N1"]
-target_sentence_field = 'Japanese'
-for q in queries:  # individual query terms
-    notes = filter_note_by_tag(search_notes(q), exclude='AnkiPy') # searches query, filters it, assign to var
-    notes = [n for n in notes if not n['fields'][target_sentence_field]['value']]
-    print(f'there are {len(notes)} notes to edit')
-    for note in notes:  # individual notes
-          edit_anki_note(note, language='Expression', sentence_field=target_sentence_field)  # starts editing it
-    print(f'finished fucking {q}')
+def main():
+    queries = ["deck:Default::Downloaded::JLPT::N5",
+               "deck:Default::Downloaded::JLPT::N4",
+               "deck:Default::Downloaded::JLPT::N3",
+               "deck:Default::Downloaded::JLPT::N2",
+               "deck:Default::Downloaded::JLPT::N1"]
+    target_sentence_field = 'Japanese'
+    for q in queries:  # individual query terms
+        notes = filter_note_by_tag(search_notes(q), exclude='AnkiPy')  # searches query, filters it, assign to var
+        notes = [n for n in notes if not n['fields'][target_sentence_field]['value']]
+        print(f'there are {len(notes)} notes to edit')
+        for note in notes:  # individual notes
+            edit_anki_note(note, language='Expression', sentence_field=target_sentence_field)  # starts editing it
+        print(f'finished fucking {q}')
--- check_duplicates.py
+++ check_duplicates.py
@@ -1,4 +1,24 @@
-# for notes/cards with the tag "Languages::Japanese::Core6K"
-# 6K = [notes for notes in tag "Languages::Japanese::Core6K"]
-# Main = [notes for notes in "Languages::Japanese::Main"]
+from anki_language_samples import url_requests
+
+SIX_K = url_requests.search_notes("tag:Languages::Japanese::Core6K")
+MAIN = url_requests.search_notes("tag:Languages::Japanese::Main")
+
+for notes in SIX_K[:10]:
+    print(notes['fields']['Word']['value'])
+
+for notes in MAIN[:10]:
+    print(notes['fields']['Word']['value'])
+
+for notes_2, notes_1 in zip(SIX_K, MAIN):
+    print(notes_2['fields']['Word']['value'], ' | ',
+          notes_1['fields']['Word']['value'])
+    break
+
+six_k = {notes['fields']['Word']['value']: notes['noteId'] for notes in SIX_K}
+main = {notes['fields']['Word']['value']: notes['noteId'] for notes in MAIN}
+duplicate = set(six_k) & set(main)
+duplicate = list(duplicate)
+
+print(duplicate[0])
+
 # duplicates = [notes['Word'] for notes in 6K if notes['Word'] in Main]
