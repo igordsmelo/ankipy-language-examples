5ef74a4ec7e12490a3ba85be9bcf656d1a690c8d
Igor <igordsmelo@gmail.com>
2023-10-17 23:53:08 -0300

    updates

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from WIP.script_ankilangsamples.utils.anki_sentences import filter_note_by_tag, 
 from WIP.script_ankilangsamples.utils.url_requests import search_notes
 
 a = 'test'
-def main():
-    queries = ["deck:Default::Downloaded::JLPT::N5",
-               "deck:Default::Downloaded::JLPT::N4",
-               "deck:Default::Downloaded::JLPT::N3",
-               "deck:Default::Downloaded::JLPT::N2",
-               "deck:Default::Downloaded::JLPT::N1"]
-    target_sentence_field = 'Japanese'
+
+
+def main(query: str or list, target_sentence_field: str = 'Japanese'):
+    """
+    Adds example sentences to cards in deck.
+    :param query:
+    :param target_sentence_field:
+    """
+    queries = str(query) if type(query) == str else query
     for q in queries:  # individual query terms
         notes = filter_note_by_tag(search_notes(q), exclude='AnkiPy')  # searches query, filters it, assign to var
         notes = [n for n in notes if not n['fields'][target_sentence_field]['value']]
@@ -16,3 +18,12 @@ def main():
         for note in notes:  # individual notes
             edit_anki_note(note, language='Expression', sentence_field=target_sentence_field)  # starts editing it
         print(f'finished fucking {q}')
+
+
+queries = ["deck:Default::Downloaded::JLPT::N5",
+           "deck:Default::Downloaded::JLPT::N4",
+           "deck:Default::Downloaded::JLPT::N3",
+           "deck:Default::Downloaded::JLPT::N2",
+           "deck:Default::Downloaded::JLPT::N1"]
+if __name__ == '__main__':
+    main(queries)
